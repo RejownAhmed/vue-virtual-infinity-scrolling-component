c89e1e144002c3922dfd3de47e15c3b0df930784
“rejownahmed” <rejownahmedzisan@gmail.com>
2024-03-09 02:49:19 +0600

    Update: App vue file

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
-import {ref} from "vue";
+import { ref } from "vue";
 
-import virtualInfinityScroll from './components/virtualInfinityScroll.vue'
+import virtualInfinityScroll from "./components/virtualInfinityScroll.vue";
 
 const startId = ref(1);
 
@@ -21,64 +21,79 @@ function generate(perPage = 10) {
   });
 }
 
-
 const items = ref([]);
 const preloader = ref(false);
 const totalItems = ref(1000);
 const deletedItems = ref([]);
 const currentPage = ref(1);
 
-const getNewItems = ()=> {
+const getNewItems = () => {
   preloader.value = true;
-  generate(50, currentPage.value).then(res=> {
-    items.value.push(...res);
-    currentPage.value++;
-  }).catch(error => {
-    console.error("Error occurred:", error);
-  }).finally(()=> preloader.value = false);
+  generate(50, currentPage.value)
+    .then((res) => {
+      items.value.push(...res);
+      currentPage.value++;
+    })
+    .catch((error) => {
+      console.error("Error occurred:", error);
+    })
+    .finally(() => (preloader.value = false));
+};
 
+const deleteItem = (item)=> {
+  items.value.splice(items.value.findIndex(i=> i.id == item.id), 1);
 }
-
 </script>
 
 <template>
   <div>
-  <virtual-infinity-scroll
-    class="scroller"
-    container-class="container"
-    :Items="items"
-    unique-key="id"
-    :total-items="totalItems"
-    :pending-state="preloader"
-    :buffer-size="5"
-    :deleted-items="deletedItems"
-    @getNewItems="getNewItems()"
-  >
-    <template #item="{ item }">
-      <div>
-        {{item.title}}
-      </div>
-    </template>
-    <template #noData>
-      Nothing found!
-    </template>
-    <template #loader>
-      Loading ..... 
-    </template>
-  </virtual-infinity-scroll>
+    <virtual-infinity-scroll
+      class="scroller"
+      container-class="container"
+      :Items="items"
+      unique-key="id"
+      :total-items="totalItems"
+      :pending-state="preloader"
+      :buffer-size="5"
+      :deleted-items="deletedItems"
+      @getNewItems="getNewItems()"
+    >
+      <template #item="{ item }">
+        <div class="item">
+          {{ item.title }}
+
+          <button @click="deleteItem(item)">Delete</button>
+        </div>
+      </template>
+      <template #noData> Nothing found! </template>
+      <template #loader> Loading ..... </template>
+    </virtual-infinity-scroll>
   </div>
 </template>
 
-<style scoped>
+<style>
 .scroller {
   min-height: 150px;
-  max-height: 200px;
-  width: 400px;
+  max-height: 300px;
+  width: 300px;
+  border: 1px solid rgb(84, 95, 85);
 }
 
 .container {
   display: flex;
   flex-direction: column;
   gap: 0.765rem;
+  padding: 20px 10px;
 }
+
+.item {
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+  justify-content: space-between;
+  border: 1px solid #999;
+  padding: 3px 10px;
+  border-radius: 10px;
+}
+
 </style>
